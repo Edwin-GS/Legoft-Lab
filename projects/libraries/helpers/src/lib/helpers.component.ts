e740d50b4eae1c53d13d4f507de3d75e53daaf33
Edwin Guillén Santana <egsant@outlook.com>
2023-09-25 23:13:17 +0200

    Primer commit, con la app client y la libreria helpers creados.

---
+++ projects/libraries/helpers/src/lib/helpers.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-helpers',
+  template: `
+    <p>
+      helpers works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class HelpersComponent {
+
+}
---
+++ projects/libraries/helpers/src/lib/helpers.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { HelpersComponent } from './helpers.component';
+
+
+
+@NgModule({
+  declarations: [
+    HelpersComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    HelpersComponent
+  ]
+})
+export class HelpersModule { }
