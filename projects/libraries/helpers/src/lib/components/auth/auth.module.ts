4453b8dcc43a02d734cf1f6ac3f4141b515de494
Andres de la Cruz <dlcrz2k@gmail.com>
2023-10-03 15:57:42 -0400

    [Create]: Register and Not Found

--- projects/libraries/helpers/src/lib/components/auth/auth.module.ts
+++ projects/libraries/helpers/src/lib/components/auth/auth.module.ts
@@ -3,21 +3,19 @@ import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import {AUTH_ROUTES} from "./auth.routes";
-import {HelpersModule} from "../../helpers.module";
-
-
+import { AUTH_ROUTES } from './auth.routes';
+import { HelpersModule } from '../../helpers.module';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    NotFoundComponent,
-    LoginComponent,
-    SignupComponent
-  ],
+  declarations: [NotFoundComponent, LoginComponent, SignupComponent],
   imports: [
     CommonModule,
     HelpersModule,
-    AUTH_ROUTES
-  ]
+    AUTH_ROUTES,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
-export class AuthModule { }
+export class AuthModule {}
