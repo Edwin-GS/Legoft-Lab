6e41e74c3c9068f0bde835be46adf0415b6068e0
Andres de la Cruz <dlcrz2k@gmail.com>
2023-10-03 23:04:09 -0400

    [Update]: auth.module.ts working

--- projects/libraries/helpers/src/lib/components/auth/auth.module.ts
+++ projects/libraries/helpers/src/lib/components/auth/auth.module.ts
@@ -1,20 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
-import {AUTH_ROUTES} from "./auth.routes";
-import {HelpersModule} from "../../helpers.module";
 import { VerifyEmailComponent } from './verify-email/verify-email.component';
-
-
+import { AUTH_ROUTES } from './auth.routes';
+import { HelpersModule } from '../../helpers.module';
 
 @NgModule({
   declarations: [
     NotFoundComponent,
     LoginComponent,
     SignupComponent,
-    VerifyEmailComponent
+    VerifyEmailComponent,
   ],
 
   imports: [
