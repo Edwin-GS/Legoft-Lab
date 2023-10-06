675ebd7455b0cbfdbb7b06ce5543975fde2e15c2
Andres de la Cruz <dlcrz2k@gmail.com>
2023-10-06 15:49:17 -0400

    Classic dashboard created

--- projects/libraries/helpers/src/lib/components/auth/auth.routes.ts
+++ projects/libraries/helpers/src/lib/components/auth/auth.routes.ts
@@ -1,40 +1,41 @@
-import {RouterModule, Routes} from '@angular/router';
-import {NotFoundComponent} from "./not-found/not-found.component";
-import {LoginComponent} from "./login/login.component";
-import {SignupComponent} from "./signup/signup.component";
-import {VerifyEmailComponent} from "./verify-email/verify-email.component";
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { VerifyEmailComponent } from './verify-email/verify-email.component';
 
 const AuthRoutes: Routes = [
-
   {
     path: '',
     title: 'Legoft',
-    component: LoginComponent
+    component: LoginComponent,
   },
   {
     path: 'signup',
     title: 'Legoft',
-    component: SignupComponent
+    component: SignupComponent,
   },
   {
     path: 'not-found',
     title: 'Legoft',
-    component: NotFoundComponent
+    component: NotFoundComponent,
   },
   {
     path: 'legoft-lab',
     loadChildren: () =>
-      import('../../../../../../applications/client/src/app/components/dashboard/dashboard.module').then(m => m.DashboardModule)
+      import(
+        '../../../../../../applications/client/src/app/components/dashboard/dashboard.module'
+      ).then((m) => m.DashboardModule),
   },
   {
     path: 'verify-email/:confirm',
     title: 'Legoft',
-    component: VerifyEmailComponent
+    component: VerifyEmailComponent,
   },
   {
     path: '**',
-    redirectTo: '/not-found'
-  }
+    redirectTo: '/not-found',
+  },
 ];
 
 export const AUTH_ROUTES = RouterModule.forChild(AuthRoutes);
