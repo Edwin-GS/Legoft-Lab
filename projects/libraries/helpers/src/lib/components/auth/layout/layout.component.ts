b27939b0bcbce92212592fdb59656569850c7c28
woodyjacques <Woodyjacques1@gmail.com>
2023-10-19 22:37:36 -0400

    La pagina principal

---
+++ projects/libraries/helpers/src/lib/components/auth/layout/layout.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-layout',
+  templateUrl: './layout.component.html',
+  styleUrls: ['./layout.component.css'],
+})
+export class LayoutComponent {
+  aul = 'assets/img/auriculares_2.png';
+  icono1 = 'assets/img/icono1.jpg';
+  icono2 = 'assets/img/icono2.jpg';
+  icono3 = 'assets/img/icono3.png';
+  servidor = 'assets/img/servidor.png';
+  inicio_fondo = 'assets/img/inicio_fondo.png';
+}
