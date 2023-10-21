a1a192f4bbe71f911b60dcd8e5bc5d702274f17e
woodyjacques <Woodyjacques1@gmail.com>
2023-10-20 23:12:16 -0400

    Página principal terminada

--- projects/libraries/helpers/src/lib/components/auth/layout/layout.component.ts
+++ projects/libraries/helpers/src/lib/components/auth/layout/layout.component.ts
@@ -6,7 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./layout.component.css'],
 })
 export class LayoutComponent {
-  aul = 'assets/img/auriculares_2.png';
+  formImage = 'assets/img/form_img.png';
+  unuser1 = 'assets/img/user_1.jpg';
+  unuser2 = 'assets/img/user_2.jpg';
+  unuser3 = 'assets/img/user_3.jpg';
+  aul = 'assets/img/serv_2.png';
   icono1 = 'assets/img/icono1.jpg';
   icono2 = 'assets/img/icono2.jpg';
   icono3 = 'assets/img/icono3.png';
